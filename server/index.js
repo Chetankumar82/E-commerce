d1f1324e41b42823a26553a52e7d7e4bb4eae115
Chetankumar82 <chetankumar8203@gmail.com>
2024-12-25 12:00:02 +0530

    .

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-dotenv.config()
 import cookieParser from 'cookie-parser'
+dotenv.config()
 import morgan from 'morgan'
 import helmet from 'helmet'
 import connectDB from './config/connectDB.js'
-import userRouter from './route/user.route.js'
+import userRouter from './route/users.route.js'
 import categoryRouter from './route/category.route.js'
 import uploadRouter from './route/upload.router.js'
 import subCategoryRouter from './route/subCategory.route.js'
@@ -18,22 +18,22 @@ import orderRouter from './route/order.route.js'
 const app = express()
 app.use(cors({
     credentials : true,
-    origin : process.env.FRONTEND_URL
+    origin: process.env.FRONTEND_URL
 }))
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan())
 app.use(helmet({
-    crossOriginResourcePolicy : false
+    crossOriginResourcePolicy: false
 }))
 
-const PORT = 8080 || process.env.PORT 
+const PORT = 8080 || process.env.PORT
 
 app.get("/",(request,response)=>{
-    ///server to client
-    response.json({
-        message : "Server is running " + PORT
-    })
+   ///server to client
+   response.json({
+    message:"Server is running " + PORT
+   })
 })
 
 app.use('/api/user',userRouter)
@@ -43,10 +43,13 @@ app.use("/api/subcategory",subCategoryRouter)
 app.use("/api/product",productRouter)
 app.use("/api/cart",cartRouter)
 app.use("/api/address",addressRouter)
-app.use('/api/order',orderRouter)
+app.use("/api/order",orderRouter)
+
 
 connectDB().then(()=>{
     app.listen(PORT,()=>{
         console.log("Server is running",PORT)
     })
-})
+});
+
+
